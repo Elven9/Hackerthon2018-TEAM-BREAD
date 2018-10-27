4e6330fe2783538a6789bd46e0db93c9df8be012
Guava <guava@mgallery.center>
2018-10-27 18:09:23 +0800

    root color

--- src/App.vue
+++ src/App.vue
@@ -13,4 +13,15 @@ export default {
 </script>
 
 <style lang="scss">
+  :root {
+    --bg_white: #ffffff;
+    --bg_brown: #754741;
+  }
+  body {
+    margin: 0;
+    #app {
+      width: 100vw;
+      height: 100vh;
+    }
+  }
 </style>
--- src/components/Home/footer.vue
+++ src/components/Home/footer.vue
@@ -15,5 +15,11 @@ export default {
 </script>
 
 <style lang="scss">
-
+  #footer-wrapper {
+    position: fixed;
+    bottom: 0;
+    width: 100vw;
+    height: 8vh;
+    background-color: var(--bg_white)
+  }
 </style>
