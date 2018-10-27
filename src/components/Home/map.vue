7cccb70f3a5d2787f47afc685a2089fccf326e3e
Guava <guava@mgallery.center>
2018-10-27 11:27:40 +0800

    map

--- src/components/Home/map.vue
+++ src/components/Home/map.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="map">
+    map
   </div>
 </template>
 
@@ -7,12 +8,26 @@
 
 export default {
   name: 'map',
+  data() {
+    return {
+      map: null
+    }
+  },
   components: {
-
+  },
+  methods: {
+    loadMapScenario() {
+        this.map = new Microsoft.Maps.Map(document.getElementById('map'), {});
+    }
+  },
+  mounted() {
+    this.loadMapScenario();
   }
 }
 </script>
 
 <style lang="scss">
-
+  #map {
+    height: 100vh;
+  }
 </style>
--- src/views/page/Home.vue
+++ src/views/page/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="home">
+    <Map />
   </div>
 </template>
 
 <script>
+import Map from '@/components/Home/map';
 
 export default {
   name: 'home',
   components: {
+    Map
   }
 }
 </script>
 
 <style lang="scss">
 </style>
-
