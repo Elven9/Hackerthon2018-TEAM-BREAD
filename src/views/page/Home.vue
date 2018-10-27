f8b0a83ecd34be7149ad058ba6139ae081aa7c81
breaddddd13 <xboxsonic1113@gmail.com>
2018-10-28 04:16:31 +0800

    add regist and login file

--- src/views/page/Home.vue
+++ src/views/page/Home.vue
@@ -13,6 +13,7 @@ import Header from '@/components/Home/Header'
 import RightList from '@/components/Home/Right'
 import Footer from '@/components/Home/Footer'
 
+
 export default {
   name: 'home',
   components: {
@@ -29,18 +30,18 @@ export default {
     loadMapScenario() {
       this.map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
     /* No need to set credentials if already passed in URL */
-        center: new Microsoft.Maps.Location(23.5, 120.58),
+    center: new Microsoft.Maps.Location(23.5, 120.58),
         customMapStyle: {
           elements: {
-            area: { fillColor: '#a8c5f0' },
-            water: { fillColor: '#007ff1' },
-            tollRoad: { fillColor: '#ff9999', strokeColor: '#ff9999' },
+            area: { fillColor: '#a8c4f0' },
+            water: { fillColor: '#529ee0' },
+            tollRoad: { fillColor: '#ffb3b3', strokeColor: '#ffb3b3' },
             arterialRoad: { fillColor: '# ffeee6', strokeColor: '#ffeee6' },
-            road: { fillColor: '#ffccb3', strokeColor: '#ffccb3' },
+            road: { fillColor: '#ffddcc', strokeColor: '#ffddcc' },
             street: { fillColor: '#e6f7ff', strokeColor: '#e6f7ff' },
             transit: { fillColor: '#000000' }
           },
-          settings: { landColor: '#b3e6ff' }
+          settings: { landColor: '#c1ebff' }
         }
       });
     }
